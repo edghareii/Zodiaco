51e11342dacb165923f8794e1f7c24e74c6c2af4
edgardo <edgar_king1@hotmail.com>
2016-09-16 23:07:31 -0700

    crearcion de proyecto Zodiaco y declaracion de variables.

---
+++ Zodiaco.java
@@ -0,0 +1,21 @@
+package practica001;
+
+public class Zodiaco {
+
+	/**
+	 * este programa calcula el signo del zodiaco 
+	 * del usuario.
+	 */
+	String Nombre;
+	String Apellidos;
+	int Edad;
+	int Dia_Nacimiento;
+	int Mes_Nacimiento;
+	int Año_Nacimiento;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
