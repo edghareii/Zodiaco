417eb8f5bfc0a0be8256b1433704301f79afd2c4
edgardo <edgar_king1@hotmail.com>
2016-09-17 00:01:40 -0700

    pedir datos por medio de la consola

--- Zodiaco.java
+++ Zodiaco.java
@@ -18,7 +18,23 @@ public class Zodiaco {
 
 	}
 	public void PedirDatos(){
-	
+		
+
+		Scanner teclear=new Scanner(System.in);
+		System.out.println("Teclear Nombre: ");
+		Nombre=teclear.nextLine();
+		System.out.println("Teclear Apellido: ");
+		Apellidos=teclear.nextLine();
+		System.out.println("Teclear Edad: ");
+		Edad=teclear.nextInt();
+		System.out.println("Teclear Dia de Nacimiento: ");
+		Dia_Nacimiento=teclear.nextInt();
+		System.out.println("Teclear Mes de Nacimiento: ");
+		Mes_Nacimiento=teclear.nextInt();
+		System.out.println("Teclear Año de Nacimiento: ");
+		Año_Nacimiento=teclear.nextInt();
+		
+			
 	}
 	public void CalcularZodiaco() {
 		
