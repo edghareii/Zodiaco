ffaabfc4d121a1ee3e8976c0b3412c6aee501375
edgardo <edgar_king1@hotmail.com>
2016-09-17 00:38:28 -0700

    Calcula el signo zodiacal y agrege una nueva variable

--- Zodiaco.java
+++ Zodiaco.java
@@ -37,7 +37,119 @@ public class Zodiaco {
 			
 	}
 	public void CalcularZodiaco() {
-		
+		//Calcular el signo zodiacal con el mes y dia de nacimiento
+
+		switch(Mes_Nacimiento){
+
+		case 1: //Enero
+			
+			if(Dia_Nacimiento>=21){
+				zoodiaco="Acuario";
+			}
+			else{
+				zoodiaco="Capricornio";
+			}
+			break;
+
+		case 2: //Febrero
+			if(Dia_Nacimiento<=19){
+				zoodiaco= "Acuario";
+			}
+			else{
+				zoodiaco= "Pisis";
+			}
+			break;
+
+		case 3://Marzo
+			if(Dia_Nacimiento <=20){
+				zoodiaco="Pisis";
+			}
+			else{
+				zoodiaco="Aries";
+			}
+			break;
+
+		case 4://Abril
+			if(Dia_Nacimiento <=20){
+				zoodiaco="Aries";
+			}
+			else{
+				zoodiaco="Tauro";
+			}
+			break;
+
+		case 5://Mayo
+			if(Dia_Nacimiento <= 20){
+				zoodiaco="Tauro";
+			}
+			else{
+				zoodiaco="Geminis";
+			}
+			break;
+
+		case 6://Junio
+			if(Dia_Nacimiento <=20){
+				zoodiaco="Geminis";
+			}
+			else{
+				zoodiaco="Cancer";
+			}
+			break;
+
+		case 7://Julio
+			if(Dia_Nacimiento<=21){
+				zoodiaco="Cancer";
+			}
+			else{
+				zoodiaco="Leo";
+			}
+			break;
+
+		case 8://Agosto
+			if(Dia_Nacimiento<=22){
+				zoodiaco="Leo";
+			}
+			else{
+				zoodiaco="Virgo";
+			}
+			break;
+
+		case 9://Septiembre
+			if(Dia_Nacimiento<=22){
+				zoodiaco="Virgo";
+			}
+			else{
+				zoodiaco="Libra";
+			}
+			break;
+
+		case 10://Octubre
+			if(Dia_Nacimiento<=22){
+				zoodiaco="Libra";
+			}
+			else{
+				zoodiaco="Escorpio";
+			}
+			break;
+		case 11://Noviembre
+			if(Dia_Nacimiento<=21){
+				zoodiaco="Escorpio";
+			}
+			else{
+				zoodiaco="Sagitario";
+			}
+			break;
+		case 12://Diciembre
+			if(Dia_Nacimiento<=21){
+				zoodiaco="Sagitario";
+			}
+			else{
+				zoodiaco="Capricornio";
+			}
+			break;
+
+		}	
+
 	}
 	public void Imprimir(){
 		
